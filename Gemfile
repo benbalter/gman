47a0bd87e076a34cfd7b326fa3675066eab93607
Ben Balter <ben.balter@github.com>
2013-11-24 19:11:35 -0500

    exclude edu domains

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'public_suffix'
+gem 'swot'
 
 group :development do
   gem "rake"
--- test/test_gman.rb
+++ test/test_gman.rb
@@ -1,7 +1,23 @@
 require 'helper'
 
-VALID = ["foo.gov", "http://foo.mil", "foo@bar.gc.ca", "foo.gov.au", "http://www.foo.gouv.fr", "foo@ci.champaign.il.us", "foo.bar.baz.gov.au", "foo@bar.gov.uk"]
-INVALID = ["foo.bar.com", "bar@foo.biz", "http://www.foo.biz", "foo.uk", "gov", "foo@k12.champaign.il.us"]
+VALID = [  "foo.gov",
+            "http://foo.mil",
+            "foo@bar.gc.ca",
+            "foo.gov.au",
+            "http://www.foo.gouv.fr",
+            "foo@ci.champaign.il.us",
+            "foo.bar.baz.gov.au",
+            "foo@bar.gov.uk"
+        ]
+
+INVALID = [ "foo.bar.com",
+            "bar@foo.biz",
+            "http://www.foo.biz",
+            "foo.uk",
+            "gov",
+            "foo@k12.champaign.il.us",
+            "foo@kii.gov.by"
+          ]
 
 class TestGman < Test::Unit::TestCase
   should "recognize government email addresses and domains" do
