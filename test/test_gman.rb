5fe9234abd62249da3e1f4bb55b84e4bfe603971
Ben Balter <ben.balter@github.com>
2013-11-25 13:27:16 -0500

    test for edus

--- test/test_gman.rb
+++ test/test_gman.rb
@@ -1,4 +1,5 @@
 require 'helper'
+require 'swot'
 
 VALID = [  "foo.gov",
             "http://foo.mil",
@@ -20,12 +21,22 @@ INVALID = [ "foo.bar.com",
           ]
 
 class TestGman < Test::Unit::TestCase
+
   should "recognize government email addresses and domains" do
     VALID.each do |test|
       assert_equal true, Gman::valid?(test), "#{test} should be detected as a valid government domain"
     end
+  end
+
+  should "not recognize non-government email addresses and domains" do
     INVALID.each do |test|
       assert_equal false, Gman::valid?(test), "#{test} should be detected as an invalid government domain"
+    end
+  end
+
+  should "not contain any educational domains" do
+    Gman.list.each do |entry|
+      assert_equal false, Swot::is_academic?(entry.name), "#{entry.name} is an academic domain"
     end
   end
 end
